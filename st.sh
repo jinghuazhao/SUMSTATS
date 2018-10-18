6e1e9a2eda9c21a64ca80093f17866259a95dbdb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-10-18 12:11:22 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -2,7 +2,7 @@
 
 git add README.*
 git commit -m "README"
-for d in .gitignore 97.snps bmi.txt.gz st.sh
+for d in .gitignore 97.snps bmi.txt.gz lz.sh st.sh
 do
    git add $d
    git commit -m "$d"
